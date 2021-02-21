9b4b9496f9e304db1d2b6c24c414744918926a7d
Ricardo Maia <rsmaia@gmail.com>
2021-02-21 15:29:01 -0300

    Closese #3

--- plugins/exchange2016/__init__.py
+++ plugins/exchange2016/__init__.py
@@ -1,7 +1,15 @@
-from plugins import browser
+from plugins.browser import Browser
 
 
-class exchange2016(browser.Browser):
+class exchange2016(Browser):
 
     def __init__(self):
         super().__init__()
+
+    def login(self, url, username, password):
+        super().openUrl(url)
+        super().fill('//*[@id="username"]', username)
+        super().fill('//*[@id="password"]', password)
+        super().click('/html/body/form/div/div[2]/div/div[9]/div')
+
+    def search_message()
